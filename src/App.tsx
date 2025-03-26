b274e428ab2f8241794abadcb8e5473f10e41f69
PixelBedrock <113065795+PixelBedrock@users.noreply.github.com>
2025-03-26 01:19:34 +0000

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+    return (
+        <View>
+            <Text>Edit src/App.tsx to change this view.</Text>
+        </View>
+    );
+}
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { registerRootComponent } from "expo";
+import App from "./App";
+
+/**
+ * Sets the initial React component to render natively in the app's
+ * root React Native view on Android, iOS, tvOS and the web.
+ */
+registerRootComponent(App);
